46f028326b6153a856d3c194d986e1217f5cb8eb
Cole Miller <cole.miller.foot@gmail.com>
2025-05-29 20:55:36 -0400

    about2.0

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('register/', views.register),
+    path('register/', views.register, name='register'),
     path('verification_form/', views.verification_form, name='verification_form'),
     path('log_in/', views.log_in),
     path('log_out/', views.log_out),
