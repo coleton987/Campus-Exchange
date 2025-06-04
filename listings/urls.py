c08c94e8d0ff6eef93368a410de4775da6a10026
Cole Miller <cole.miller.foot@gmail.com>
2025-06-04 17:59:00 -0400

    working on delete entry

--- listings/urls.py
+++ listings/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('product_details/<int:id>/', views.product_details, name='product_details'),
     path('upload/', views.file_upload, name='file_upload'),
     path('edit/<int:id>/', views.edit_listing, name='edit_listing'),
+    path('delete/<int:id>/', views.delete_listing, name='delete_listing'),
     path('remove-image/', views.remove_image, name='remove_image'),  
     path('update-photo-order/', views.update_photo_order, name='update_photo_order'),
 ]
